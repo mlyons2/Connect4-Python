fc3559ae787272aff68e6996f6a66bf16f4363c2
chasepg0821 <91268417+chasepg0821@users.noreply.github.com>
2021-11-11 11:15:30 -0500

    Chanes for Issue#3
    Changed the font to comicsans

--- connect4.py
+++ connect4.py
@@ -83,13 +83,13 @@ height = (ROW_COUNT+1) * SQUARESIZE
 
 size = (width, height)
 
-RADIUS = int(SQUARESIZE/2 - 3)
+RADIUS = int(SQUARESIZE/2 - 5)
 
 screen = pygame.display.set_mode(size)
 draw_board(board)
 pygame.display.update()
 
-myfont = pygame.font.SysFont("monospace", 75)
+myfont = pygame.font.SysFont("comicsans", 75)
 
 while not game_over:
 
